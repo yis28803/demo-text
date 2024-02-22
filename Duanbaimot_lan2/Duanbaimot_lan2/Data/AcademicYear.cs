49c5978f8800c4ae262785c833e1222455cfaac4
BaNghia <yis28830@gmail.com>
2024-02-22 20:57:51 +0700

    Add 22/2

--- Duanbaimot_lan2/Duanbaimot_lan2/Data/AcademicYear.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Duanbaimot_lan2.Data
-{
-    public class AcademicYear
-    {
-        public int AcademicYearID { get; set; }
-        public int Year { get; set; }
-        // Other properties...
-    }
-}
